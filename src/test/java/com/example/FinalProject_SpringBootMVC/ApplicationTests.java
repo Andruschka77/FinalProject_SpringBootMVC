e3be374aafca6076dbf320443edd6e93b6a0b515
Andruschka77 <kulikovandr567@gmail.com>
2026-03-17 15:03:24 +0300

    Initial commit

---
+++ src/test/java/com/example/FinalProject_SpringBootMVC/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinalProject_SpringBootMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
